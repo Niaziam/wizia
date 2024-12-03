5e4cbe7505d73777ff2d548f195d19429ba60dcb
Niaziam <niazuddintanim20@gmail.com>
2024-12-03 22:31:04 +0600

    problem solving

--- src/components/FeatureHighlight.jsx
+++ src/components/FeatureHighlight.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const FeatureHighlight = () => {
 	return (
 		<section className='px-10'>
-			<div className="bg-[url('ImgV.png')] md:bg-[url('ImgMirror.png')] bg-no-repeat bg-cover rounded-2xl lg:pl-[500px] p-5 md:p-20 ">
+			<div className="bg-[url('/ImgV.png')] md:bg-[url('/ImgMirror.png')] bg-no-repeat bg-cover rounded-2xl lg:pl-[500px] p-5 md:p-20 ">
 				<h1 className='text-white font-semibold text-4xl mt-5'>Train your aiDR on your...</h1>
 				<p className='text-secondary font-semibold text-4xl mt-5'><i>prefered email st|</i></p>
 				<p className='text-text text-xl mt-5'>You are in control. Train your aiDR on <br className='hidden lg:flex' /> elements of your Marketing strategy.</p>
